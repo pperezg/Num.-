87a9c5166b334623d12947973dabbfe544a23ab0
pperezg <52933581+pperezg@users.noreply.github.com>
2021-05-18 11:11:03 -0500

    Add files via upload

---
+++ vandermonde2.m
@@ -0,0 +1,13 @@
+function [X f] = vandermonde2(datos)
+[m, n] = size(datos);
+b = datos(:,2);
+A = zeros(m);
+for i=1:m
+    x = datos(i,1);
+    for j=m-1:-1:0
+        A(i,m-j)= x^j;
+    end
+end
+X = A\b; % coeficientes
+f = poly2sym(X.'); % funcion
+end
